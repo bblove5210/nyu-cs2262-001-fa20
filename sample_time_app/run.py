ca946c63f25abf0cbb639553ab6486b15bfbaa7e
bblove5210 <1714364572qq@gmail.com>
2025-09-25 05:37:05 -0400

    updated Dockerfile and run.py

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,10 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+from datetime import datetime
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+@app.route('/time')
+def get_time():
+    return {'time': datetime.now().strftime("%H:%M:%S")}
 
 
 app.run(host='0.0.0.0',
